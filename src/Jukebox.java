7655e87d505a9a9c12bc4e41432f8413ad86b73b
11002084 <League@AdministorsiMac.attlocal.net>
2018-11-12 19:54:47 -0800

    Jukebox

--- src/Jukebox.java
+++ src/Jukebox.java
@@ -3,7 +3,8 @@
  *    Level 1
  */
 
-
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
@@ -11,7 +12,10 @@ import java.net.URL;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
 import javazoom.jl.player.advanced.AdvancedPlayer;
@@ -19,27 +23,56 @@ import javazoom.jl.player.advanced.AdvancedPlayer;
 /* 1. Download the JavaZoom jar from here: http://bit.ly/javazoom
  * 2. Right click your project and add it as an External JAR (Under Java Build Path > Libraries).*/
 
-public class Jukebox implements Runnable {
-
+public class Jukebox implements Runnable, ActionListener {
+	JFrame frame = new JFrame();
+	JButton button1 = new JButton();
+	JPanel panel = new JPanel();
+	JLabel label1 = new JLabel();
+	JButton button2 = new JButton();
+	JLabel label2 = new JLabel();
+	JButton button3 = new JButton();
+	JLabel label3 = new JLabel();
+	Song song=new Song("mario.mp3");
+	
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Jukebox());
 	}
 
-           public void run() {
+	public void run() {
 
 		// 3. Find an mp3 on your computer or on the Internet.
 		// 4. Create a Song
 
 		// 5. Play the Song
-
+	
 		/*
-		 * 6. Create a user interface for your Jukebox so that the user can to
-		 * choose which song to play. You can use can use a different button for
-		 * each song, or a picture of the album cover. When the button or album
-		 * cover is clicked, stop the currently playing song, and play the one
-		 * that was selected.
+		 * 6. Create a user interface for your Jukebox so that the user can to choose
+		 * which song to play. You can use can use a different button for each song, or
+		 * a picture of the album cover. When the button or album cover is clicked, stop
+		 * the currently playing song, and play the one that was selected.
 		 */
-          }
+		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		label1=loadImage("happybirthday.jpg");
+		button1.setText("Happy Birthday");
+		button1.addActionListener(this);
+		
+		label2=loadImage("mariophoto.png");
+		button2.setText("Super Mario Bros Theme Song");
+		button2.addActionListener(this);
+		label3=loadImage("jawsphoto.jpg");
+		button3.setText("Jaws Theme Song");
+		button3.addActionListener(this);
+		panel.add(button1);
+		panel.add(button2);
+		panel.add(button3);
+		panel.add(label1);
+		panel.add(label2);
+		panel.add(label3);
+		frame.add(panel);
+		frame.pack();
+	}
+
 	/* Use this method to add album covers to your Panel. */
 	private JLabel loadImage(String fileName) {
 		URL imageURL = getClass().getResource(fileName);
@@ -47,7 +80,32 @@ public class Jukebox implements Runnable {
 		return new JLabel(icon);
 	}
 
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+	if (e.getSource()==button1) {
+		song.stop();
+		 song = new Song("happybirthdaysong.mp3");
+		song.play();
+	}
+	else if (e.getSource()==button2) {
+		song.stop();
+		 song = new Song("mario.mp3");
+		song.play();
 }
+	else  if (e.getSource()==button3) {
+		song.stop();
+		 song = new Song("jaws.mp3");
+		song.play();
+}
+	else {
+	System.out.println("NOT WORKING!!!");	
+	}
+	}
+	
+	}
+
+
 
 class Song {
 
@@ -56,18 +114,20 @@ class Song {
 	private AdvancedPlayer mp3Player;
 	private InputStream songStream;
 
-	/**
-	 * Songs can be constructed from files on your computer or Internet
-	 * addresses.
-	 * 
-	 * Examples: <code> 
-	 * 		new Song("everywhere.mp3"); 	//from default package 
-	 * 		new Song("/Users/joonspoon/music/Vampire Weekend - Modern Vampires of the City/03 Step.mp3"); 
-	 * 		new	Song("http://freedownloads.last.fm/download/569264057/Get%2BGot.mp3"); 
-	 * </code>
-	 */
+	/*
+	 ** Songs can be constructed from files on your computer or Internet addresses.
+	 ****
+	 ***** Examples: <code> new Song("everywhere.mp3"); //from default package new
+	 * Song("/Users/joonspoon/music/Vampire Weekend - Modern Vampires of the City/03 Step.mp3"
+	 * ); new Song("http://freedownloads.last.fm/download/569264057/Get%2BGot.mp3");
+	 ********* </code>
+	 **********/
 	public Song(String songAddress) {
 		this.songAddress = songAddress;
+	}
+
+	public Song() {
+		// TODO Auto-generated constructor stub
 	}
 
 	public void play() {
@@ -133,4 +193,3 @@ class Song {
 		}
 	}
 }
-
